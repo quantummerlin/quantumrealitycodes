57bb4ccb21df606cb0ef12c522142e5c42fea321
quantummerlin <quantummerlin@users.noreply.github.com>
2026-03-05 18:47:53 +1100

    Rewrite Amplify section: group intention focus, Telegram link, daily code tips, pain points, prominent science links

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Quantum Reality Codes PWA Service Worker v10.0
 // Optimised for offline-first with network-first for HTML, CSS, and JS
 
-const CACHE_NAME = 'reality-codes-v15';
+const CACHE_NAME = 'reality-codes-v16';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
